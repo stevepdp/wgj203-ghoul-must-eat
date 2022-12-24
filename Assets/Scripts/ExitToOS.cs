d231b0f4498be3e429d7219f671441b69bd138a5
Stephen Pearce <hello@stevepdp.dev>
2022-12-24 01:53:30 +0000

    Improve main menu
    - Replace "ghoul speak" with legible instructions
    - Simplify menu options and add an OS exit button
    - Fix broken scene flow

---
+++ Assets/Scripts/ExitToOS.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitToOS : MonoBehaviour
+{
+#if UNITY_STANDALONE
+    [SerializeField] private Text exitButtonText;
+#endif
+
+    public void Awake()
+    {
+#if UNITY_EDITOR || UNITY_WEBGL
+        gameObject.SetActive(false);
+#endif
+#if UNITY_STANDALONE_WIN
+        exitButtonText.text = "Exit to Windows";
+#elif UNITY_STANDALONE_OSX
+        exitButtonText.text = "Exit to macOS";
+#elif UNITY_STANDALONE_LINUX
+        exitButtonText.text = "Exit to Linux";
+#endif
+    }
+
+#if UNITY_STANDALONE
+    public void ExitToDesktop()
+    {
+        Application.Quit();
+    }
+#endif
+}
