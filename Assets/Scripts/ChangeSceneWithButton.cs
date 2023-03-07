f829218aac3270fa4a364cb453f7a0435d07117d
Stephen Pearce <hello@stevepdp.dev>
2023-03-07 00:29:32 +0000

    refactor: cut down ChangeSceneWithButton script

--- Assets/Scripts/ChangeSceneWithButton.cs
+++ Assets/Scripts/ChangeSceneWithButton.cs
@@ -1,12 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ChangeSceneWithButton : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
+    public void LoadScene(string name) => SceneManager.LoadScene(name);
 }
