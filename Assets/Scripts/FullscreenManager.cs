88382cde7e536721a70427f9ff6c3057df2c8beb
Stephen Pearce <hello@stevepdp.dev>
2023-04-26 00:11:14 +0100

    feat(fullscreen): permit native resolution in fullscreen view
    This commit also allows for more keyboard combos to enter/exit
    fullscreen mode.

--- Assets/Scripts/FullscreenManager.cs
+++ Assets/Scripts/FullscreenManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class FullscreenManager : MonoBehaviour
 {
+#if PLATFORM_STANDALONE
     static FullscreenManager instance;
     public static FullscreenManager Instance
     {
@@ -19,8 +20,8 @@ public class FullscreenManager : MonoBehaviour
         }
     }
 
-    const int WIDTH = 1280;
-    const int HEIGHT = 720;
+    const int DEFAULT_WIDTH = 1280;
+    const int DEFAULT_HEIGHT = 720;
 
     void Awake()
     {
@@ -47,8 +48,11 @@ public class FullscreenManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
             DisableFullScreen();
 
-        if ((Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.F)) ||
+        if ((Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Return)) ||
+            (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.F)) ||
+            (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Return)) ||
             (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F)) ||
+            (Input.GetKey(KeyCode.LeftCommand) && Input.GetKeyDown(KeyCode.Return)) ||
             (Input.GetKey(KeyCode.LeftCommand) && Input.GetKeyDown(KeyCode.F))) {
 
             if (Screen.fullScreen)
@@ -58,7 +62,8 @@ public class FullscreenManager : MonoBehaviour
         }
     }
 
-    void DisableFullScreen() => Screen.SetResolution(WIDTH, HEIGHT, false);
+    void DisableFullScreen() => Screen.SetResolution(DEFAULT_WIDTH, DEFAULT_HEIGHT, false);
 
-    void EnableFullScreen() => Screen.SetResolution(WIDTH, HEIGHT, true);
+    void EnableFullScreen() => Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+#endif
 }
