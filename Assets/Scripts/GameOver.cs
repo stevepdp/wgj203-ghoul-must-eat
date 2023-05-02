9624246b5a69f96e891d9ff940aa1654d028264b
Stephen Pearce <hello@stevepdp.dev>
2023-05-02 12:53:02 +0100

    feat(ui): fix UI inconsistancies
    Addressing inconsistant fonts, text and button placements.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,8 +6,8 @@ public class GameOver : MonoBehaviour
     [SerializeField] Text ghoulCountText;
     [SerializeField] Text survivedTimeText;
 
-    const string GHOUL_COUNT_PREFIX = "You grew a horde of ";
-    const string SURVIVE_TIME_PREFIX = "You survived: ";
+    const string GHOUL_COUNT_PREFIX = "YOU GREW A HORDE OF: ";
+    const string SURVIVE_TIME_PREFIX = "YOU SURVIVED: ";
 
     void Start()
     {
@@ -20,9 +20,9 @@ public class GameOver : MonoBehaviour
         {
             string ghoulCountSuffix = "";
             if (GameManager.Instance.TotalGhouls <= 1)
-                ghoulCountSuffix = " ghoul.";
+                ghoulCountSuffix = " GHOUL.";
             else
-                ghoulCountSuffix = " ghouls.";
+                ghoulCountSuffix = " GHOULS.";
 
             if (ghoulCountText != null)
                 ghoulCountText.text = GHOUL_COUNT_PREFIX + GameManager.Instance.TotalGhouls.ToString() + ghoulCountSuffix;
